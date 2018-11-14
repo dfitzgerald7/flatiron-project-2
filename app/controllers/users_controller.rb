071b34b2f05b2867052ade371a590c2a117d3cea
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-11-14 15:46:42 -0500

    users can now sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,42 @@
 class UsersController < ApplicationController
 
-  get "/signup" do 
+  get "/signup" do
+    #user is not logged in
+    unless Helper.logged_in(session)
+      erb :"/users/signup"
+    else
+      redirect "/" #check this route later
+    end
+  end
 
- end
+  post "/signup" do
+    #instantiate this user
+    user = User.new(params)
+    #try to save the user
+    if user.save
+    #if the user saves,
+      session["user_id"] = user.id
+      binding.pry
+      redirect "/teams"
+    else
+      #diplsay error message
+      redirect "/signup"
+    end
+  end
+
+  get "/login" do
+    unless Helper.logged_in(session)
+      erb :"/users/login"
+    else
+      redirect "/"
+  end
+
+  post "/login" do
+
+  end
+
+  get "logout" do
+
+  end
 
 end
