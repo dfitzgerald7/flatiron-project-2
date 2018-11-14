63cb0811a11c296f7bc5e01d52b48f56183e16a1
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-11-14 14:44:52 -0500

    Added migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :teams
   has_secure_password
-  validates :username, :email, presence :true
+  validates :username, :email, presence: true
 end
---
+++ db/migrate/20181114194034_user_teams.rb
@@ -0,0 +1,8 @@
+class UserTeams < ActiveRecord::Migration
+  def change
+    create_table :user_teams do |t|
+      t.integer :user_id
+      t.integer :team_id
+    end
+  end
+end
